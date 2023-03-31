0c5c4e32f40f5fce5dbb621d4fda530231386aba
Laoshin <Laoshin@LaoShin>
2023-03-31 16:10:12 -0300

    correção de codigo

--- projetoteste2java/src/adicionando/noGit/Pessoa.java
+++ projetoteste2java/src/adicionando/noGit/Pessoa.java
@@ -10,6 +10,14 @@ public class Pessoa {
 	
 	private String dataNascimento;
 
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 	public int getIdade() {
 		return idade;
 	}
